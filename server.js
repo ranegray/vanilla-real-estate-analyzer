4c98e7c9549a0d698b9b43bee3c5b943ba476c6f
Rane Gray <ranegray830@gmail.com>
2023-04-10 21:51:24 +0200

    Added initial GET route and tested end point good

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ import cors from "cors";
 import pg from "pg";
 import dotenv from "dotenv";
 
+dotenv.config()
 const port = process.env.PORT || 3000;
+const connectionString = process.env.DATABASE_URL
+
+const { Pool } = pg;
+const pool = new Pool({ connectionString })
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('test is working, server is on')
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
