6c9883b256e4454cc731732fe3e82fbc21b7c2b3
Rane Gray <ranegray830@gmail.com>
2023-04-10 18:06:58 +0200

    Installed dependancies and setup initial server

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
