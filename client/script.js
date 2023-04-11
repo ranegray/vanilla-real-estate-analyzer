9d7abda70bbf699f13f8ce54d4d91c6bc2ca6015
Rane Gray <ranegray830@gmail.com>
2023-04-11 18:24:34 +0200

    Added fetch to get all properties and styled property cards

--- client/script.js
+++ client/script.js
@@ -1,4 +1,44 @@
 const submitBtn = document.querySelector("button");
+const propertiesEl = document.querySelector("#property-list");
+
+fetch("http://localhost:3000/api/properties")
+  .then((response) => response.json())
+  .then((data) => {
+    let formatCurrency = new Intl.NumberFormat(undefined, {
+      style: 'currency',
+      currency: 'USD'
+    });
+
+    console.log(data);
+    data.data.forEach((property) => {
+      const { name, address, purchase_price, down_payment, loan_length, interest_rate, rental_income, expenses } = property
+      // const cashFlow = calculateCashFlow()
+      const propertyCard = document.createElement("div");
+      propertyCard.classList.add(
+        "text-white",
+        "border-2",
+        "px-2",
+        "py-2",
+        "my-1",
+        "rounded-md",
+        "flex",
+        "justify-between",
+        "border-neutral-700"
+      );
+      propertyCard.innerHTML = `
+        <div>
+          <h2 class="font-semibold">${name}</h2>
+          <h3>${address}</h3>
+          <p>${formatCurrency.format(purchase_price)}</p>
+        </div>
+        <div>
+          <h3 class="font-semibold">Cash Flow</h3>
+          <p>real cash flow</p>
+        </div>
+      `;
+      propertiesEl.append(propertyCard);
+    });
+  });
 
 submitBtn.addEventListener("click", async (e) => {
   e.preventDefault();
@@ -10,14 +50,13 @@ submitBtn.addEventListener("click", async (e) => {
     formDataObj[key] = value;
   }
 
-//   fetch("/api/properties", {
-//     method: "post",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(formDataObj)
-//   });
+  //   fetch("/api/properties", {
+  //     method: "post",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(formDataObj)
+  //   });
 
-// add a copy straight to the DOM. I think its called optimistic loading or something like that.
-  
+  // add a copy straight to the DOM. I think its called optimistic loading or something like that.
 
   console.log(formDataObj);
 });
