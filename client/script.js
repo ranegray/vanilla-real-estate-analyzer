e2691862885b2dfa2c56746eb5a907265793c64c
Rane Gray <ranegray830@gmail.com>
2023-04-11 19:18:40 +0200

    Added POST route

--- client/script.js
+++ client/script.js
@@ -5,13 +5,22 @@ fetch("http://localhost:3000/api/properties")
   .then((response) => response.json())
   .then((data) => {
     let formatCurrency = new Intl.NumberFormat(undefined, {
-      style: 'currency',
-      currency: 'USD'
+      style: "currency",
+      currency: "USD",
     });
 
     console.log(data);
     data.data.forEach((property) => {
-      const { name, address, purchase_price, down_payment, loan_length, interest_rate, rental_income, expenses } = property
+      const {
+        name,
+        address,
+        purchase_price,
+        down_payment,
+        loan_length,
+        interest_rate,
+        rental_income,
+        expenses,
+      } = property;
       // const cashFlow = calculateCashFlow()
       const propertyCard = document.createElement("div");
       propertyCard.classList.add(
@@ -50,13 +59,38 @@ submitBtn.addEventListener("click", async (e) => {
     formDataObj[key] = value;
   }
 
-  //   fetch("/api/properties", {
-  //     method: "post",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify(formDataObj)
-  //   });
+  fetch("http://localhost:3000/api/properties", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formDataObj),
+  });
 
   // add a copy straight to the DOM. I think its called optimistic loading or something like that.
+  const propertyCard = document.createElement("div");
+  propertyCard.classList.add(
+    "text-white",
+    "border-2",
+    "px-2",
+    "py-2",
+    "my-1",
+    "rounded-md",
+    "flex",
+    "justify-between",
+    "border-neutral-700"
+  );
+  propertyCard.innerHTML = `
+    <div>
+      <h2 class="font-semibold">${formDataObj.name}</h2>
+      <h3>${formDataObj.address}</h3>
+      <p>${formatCurrency.format(formDataObj.purchase_price)}</p>
+    </div>
+    <div>
+      <h3 class="font-semibold">Cash Flow</h3>
+      <p>real cash flow</p>
+    </div>
+  `;
+  propertiesEl.append(propertyCard);
 
+  form.reset();
   console.log(formDataObj);
 });
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 import pg from "pg";
 const { Pool } = pg;
 
 const port = process.env.PORT || 3000;
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
 
 const app = express();
@@ -20,15 +20,23 @@ app.get("/", (req, res) => {
   res.json({ message: "server is connected" });
 });
 app.get("/api/properties", async (req, res) => {
-  const properties = await pool.query('SELECT * FROM properties')
-  res.json({data: properties.rows})
+  const properties = await pool.query("SELECT * FROM properties");
+  res.json({ data: properties.rows });
 });
 app.get("/api/properties/:id", async (req, res) => {
-  const property = await pool.query('SELECT * FROM properties WHERE id = $1', [req.params.id])
-  res.json({data: property.rows[0]})
+  const property = await pool.query("SELECT * FROM properties WHERE id = $1", [
+    req.params.id,
+  ]);
+  res.json({ data: property.rows[0] });
 });
-app.post("/api/properties", (req, res) => {});
+app.post("/api/properties", (req, res) => {
+  pool.query(
+    "INSERT INTO properties (name, address, purchase_price, interest_rate, down_payment, loan_length, rental_income, expenses, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW(), NOW())",
+    [req.body.name, req.body.address, req.body.purchase_price, req.body.interest_rate, req.body.down_payment, req.body.loan_length, req.body.rental_income, req.body.expenses]
+  );
+  res.json({ message: "success" });
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
